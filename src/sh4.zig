244639813757256817f9c8b5440079c367f6cf55
Senryoku <maretverdant@gmail.com>
2023-12-24 23:09:14 +0100

    Missed some obvious inlining

--- src/sh4.zig
+++ src/sh4.zig
@@ -1155,11 +1155,11 @@ pub const SH4 = struct {
     }
 };
 
-fn zero_extend(d: anytype) u32 {
+inline fn zero_extend(d: anytype) u32 {
     return @intCast(d);
 }
 
-fn sign_extension_u8(d: u8) i32 {
+inline fn sign_extension_u8(d: u8) i32 {
     if ((d & 0x80) == 0) {
         return @bitCast(0x000000FF & zero_extend(d));
     } else {
@@ -1167,7 +1167,7 @@ fn sign_extension_u8(d: u8) i32 {
     }
 }
 
-fn sign_extension_u12(d: u12) i32 {
+inline fn sign_extension_u12(d: u12) i32 {
     if ((d & 0x800) == 0) {
         return @bitCast(0x00000FFF & zero_extend(d));
     } else {
@@ -1175,7 +1175,7 @@ fn sign_extension_u12(d: u12) i32 {
     }
 }
 
-fn sign_extension_u16(d: u16) i32 {
+inline fn sign_extension_u16(d: u16) i32 {
     if ((d & 0x8000) == 0) {
         return @bitCast(0x0000FFFF & zero_extend(d));
     } else {
@@ -1183,7 +1183,7 @@ fn sign_extension_u16(d: u16) i32 {
     }
 }
 
-fn as_i32(val: u32) i32 {
+inline fn as_i32(val: u32) i32 {
     return @bitCast(val);
 }
 
