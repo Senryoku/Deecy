67a25de32d9c776132981e948788acc6a5365d22
Senryoku <maretverdant@gmail.com>
2024-11-15 14:30:31 +0100

    Cleanup

--- src/jit/sh4_virtual_address_space_linux.zig
+++ src/jit/sh4_virtual_address_space_linux.zig
@@ -54,9 +54,6 @@ pub fn deinit(self: *@This()) void {
         std.posix.munmap(item);
     }
     self.mirrors.deinit();
-    std.posix.unlinkat(self.vram, "vram", 0) catch |err| std.debug.print(termcolor.yellow("Failed to unlink vram: {}\n"), .{err});
-    std.posix.unlinkat(self.ram, "ram", 0) catch |err| std.debug.print(termcolor.yellow("Failed to unlink ram: {}\n"), .{err});
-    std.posix.unlinkat(self.boot, "boot", 0) catch |err| std.debug.print(termcolor.yellow("Failed to unlink boot: {}\n"), .{err});
     std.posix.munmap(self.base);
 }
 
@@ -84,7 +81,6 @@ fn mirror(self: *@This(), fd: std.posix.fd_t, size: u64, offset: u64) !void {
 }
 
 fn sigsegv_handler(sig: i32, info: *const std.posix.siginfo_t, context_ptr: ?*anyopaque) callconv(.C) void {
-    std.debug.print("sigsegv_handler\n", .{});
     const context: *std.posix.ucontext_t = @alignCast(@ptrCast(context_ptr.?));
     switch (sig) {
         std.posix.SIG.SEGV => {
@@ -96,6 +92,7 @@ fn sigsegv_handler(sig: i32, info: *const std.posix.siginfo_t, context_ptr: ?*an
 
             if (fault_address >= space_base and fault_address < space_base + GLOBAL_VIRTUAL_ADDRESS_SPACE_BASE.?.len) {
                 const addr: u32 = @truncate(fault_address - space_base);
+                std.log.scoped(.sh4_jit).debug("  Patching Acces: @ {X} - {X:0>8}  \n", .{ fault_address, addr });
 
                 const start_patch = context.mcontext.gregs[std.posix.REG.RIP];
                 var end_patch = start_patch;
@@ -104,39 +101,15 @@ fn sigsegv_handler(sig: i32, info: *const std.posix.siginfo_t, context_ptr: ?*an
                 if (@as(*u8, @ptrFromInt(end_patch)).* == 0x66)
                     end_patch += 1;
 
-                var rex: Architecture.REX = .{};
-                if (0xF0 & @as(*u8, @ptrFromInt(end_patch)).* == 0x40) {
-                    rex = @bitCast(@as(*u8, @ptrFromInt(end_patch)).*);
+                // Skip REX
+                if (0xF0 & @as(*u8, @ptrFromInt(end_patch)).* == 0x40)
                     end_patch += 1;
-                }
 
-                const mov_instruction: [*]u8 = @ptrFromInt(end_patch);
-                var modrm: Architecture.MODRM = @bitCast(mov_instruction[1]);
+                // Skip OF prefix
+                if (@as(*u8, @ptrFromInt(end_patch)).* == 0x0F)
+                    end_patch += 1;
 
-                var access_type: enum(u1) { read = 0, write = 1 } = .read;
-
-                switch (mov_instruction[0]) {
-                    0x8A, 0x8B => access_type = .read,
-                    0x88, 0x89 => access_type = .write,
-                    0x0F => {
-                        switch (mov_instruction[1]) {
-                            //  mov       movzx
-                            0x8A, 0x8B, 0xB6, 0xB7 => access_type = .read,
-                            //  mov       movq
-                            0x88, 0x89, 0x7E => access_type = .write,
-                            else => {
-                                std.debug.print("Unhandled instruction: {X}\n", .{mov_instruction[1]});
-                                return signal_not_handled();
-                            },
-                        }
-                        end_patch += 1;
-                        modrm = @bitCast(mov_instruction[2]);
-                    },
-                    else => {
-                        std.debug.print("Unhandled instruction: {X}\n", .{mov_instruction[0]});
-                        return signal_not_handled();
-                    },
-                }
+                const modrm: Architecture.MODRM = @bitCast(@as(*u8, @ptrFromInt(end_patch + 1)).*);
 
                 switch (modrm.mod) {
                     .indirect => end_patch += 2,
@@ -156,44 +129,11 @@ fn sigsegv_handler(sig: i32, info: *const std.posix.siginfo_t, context_ptr: ?*an
                     },
                 }
 
-                std.debug.print("  Access Violation: {s: <5} @ {X} - {X:0>8}  \n", .{ @tagName(access_type), fault_address, addr });
-                for ([_]usize{
-                    std.posix.REG.RDI,
-                    std.posix.REG.RSI,
-                    std.posix.REG.RBP,
-                    std.posix.REG.RSP,
-                    std.posix.REG.RBX,
-                    std.posix.REG.RDX,
-                    std.posix.REG.RCX,
-                    std.posix.REG.RAX,
-                }, [_][]const u8{
-                    "RDI",
-                    "RSI",
-                    "RBP",
-                    "RSP",
-                    "RBX",
-                    "RDX",
-                    "RCX",
-                    "RAX",
-                }, 0..) |reg, name, rn| {
-                    std.debug.print("  {s}={X:0>16}  R{d: <2}={X:0>16}\n", .{ name, context.mcontext.gregs[reg], 8 + rn, context.mcontext.gregs[rn] });
-                }
-
-                // const aligned_addr: []align(std.mem.page_size) u8 = @as([*]align(std.mem.page_size) u8, @ptrFromInt(start_patch & ~(@as(u64, std.mem.page_size) - 1)))[0..std.mem.page_size];
-                // std.posix.mprotect(aligned_addr, std.posix.PROT.READ | std.posix.PROT.WRITE | std.posix.PROT.EXEC) catch |err| std.debug.print(termcolor.yellow("Failed to mprotect: {}\n"), .{err});
-
                 // Patch out the mov and jump, we'll always execute the fallback from now on.
-                std.debug.print("  Was: {X:0>2}\n", .{@as([*]u8, @ptrFromInt(start_patch))[0..(end_patch - start_patch)]});
                 Architecture.convert_to_nops(@as([*]u8, @ptrFromInt(start_patch))[0..(end_patch - start_patch)]);
-                std.debug.print("  Now: {X:0>2}\n", .{@as([*]u8, @ptrFromInt(start_patch))[0..(end_patch - start_patch)]});
-
-                // std.posix.mprotect(aligned_addr, std.posix.PROT.READ | std.posix.PROT.EXEC) catch |err| std.debug.print(termcolor.yellow("Failed to mprotect: {}\n"), .{err});
-
-                std.debug.print("start_patch: {X:0>8}, end_patch: {X:0>8}\n", .{ start_patch, end_patch });
-                //std.debug.print("  Around: {X:0>2}\n", .{@as([*]u8, @ptrFromInt(start_patch - 0x100))[0..(end_patch + 0x200 - start_patch)]});
 
                 // Skip patched instructions. Not strictly necessary.
-                // context.mcontext.gregs[std.posix.REG.RIP] = end_patch;
+                context.mcontext.gregs[std.posix.REG.RIP] = end_patch;
 
                 return;
             }
