18674b531f1e7ba32b9f2b179370822751842990
Senryoku <maretverdant@gmail.com>
2025-03-13 00:06:39 +0100

    Simplify translate_intruction_address

--- src/dreamcast/sh4.zig
+++ src/dreamcast/sh4.zig
@@ -1120,48 +1120,36 @@ pub const SH4 = struct {
             self.jump_to_exception(.InstructionAddressError);
             return self.pc & 0x1FFF_FFFF;
         }
+        if (virtual_addr & 0x8000_0000 != 0) return virtual_addr & 0x1FFF_FFFF;
 
         const LRUIMasks = [4]u6{ 0b000111, 0b011001, 0b101010, 0b110100 };
         const LRUIValues = [4]u6{ 0b000000, 0b100000, 0b010100, 0b001011 };
         const mmucr = self.p4_register(mmu.MMUCR, .MMUCR);
 
-        switch (virtual_addr) {
-            // Operand Cache RAM Mode
-            0x7C00_0000...0x7FFF_FFFF,
-            // P1
-            0x8000_0000...0x9FFF_FFFF,
-            // P2
-            0xA000_0000...0xBFFF_FFFF,
-            // P4
-            0xE000_0000...0xFFFF_FFFF,
-            => return virtual_addr & 0x1FFF_FFFF,
-            else => {
-                if (EmulateITLB) {
-                    // Search ITLB
-                    const check_asid = !mmucr.sv or self.sr.md == 0;
-                    const asid = self.read_p4_register(mmu.PTEH, .PTEH).asid;
-                    const vpn: u22 = @truncate(virtual_addr >> 10);
-                    for (self.itlb, 0..) |entry, idx| {
-                        // NOTE: Here we assume only one entry will match, TLB multiple hit exception isn't emulated (It's fatal anyway).
-                        if (entry.match(check_asid, asid, vpn)) {
-                            if (self.sr.md == 0 and entry.pr.privileged()) {
-                                self.report_address_exception(virtual_addr);
-                                self.jump_to_exception(.InstructionTLBProtectionViolation);
-                                return self.pc & 0x1FFF_FFFF;
-                            }
-
-                            // Update LRUI bits (determine which ITLB entry to evict on ITLB miss)
-                            mmucr.lrui &= LRUIMasks[idx];
-                            mmucr.lrui |= LRUIValues[idx];
-
-                            const physical_address = entry.translate(virtual_addr);
-                            mmu_log.debug("ITLB Hit: {x:0>8} -> {x:0>8}", .{ virtual_addr, physical_address });
-                            mmu_log.debug("  Entry {d}: {any}", .{ idx, entry });
-                            return physical_address & 0x1FFF_FFFF;
-                        }
+        if (EmulateITLB) {
+            // Search ITLB
+            const check_asid = !mmucr.sv or self.sr.md == 0;
+            const asid = self.read_p4_register(mmu.PTEH, .PTEH).asid;
+            const vpn: u22 = @truncate(virtual_addr >> 10);
+            for (self.itlb, 0..) |entry, idx| {
+                // NOTE: Here we assume only one entry will match, TLB multiple hit exception isn't emulated (It's fatal anyway).
+                if (entry.match(check_asid, asid, vpn)) {
+                    if (self.sr.md == 0 and entry.pr.privileged()) {
+                        self.report_address_exception(virtual_addr);
+                        self.jump_to_exception(.InstructionTLBProtectionViolation);
+                        return self.pc & 0x1FFF_FFFF;
                     }
+
+                    // Update LRUI bits (determine which ITLB entry to evict on ITLB miss)
+                    mmucr.lrui &= LRUIMasks[idx];
+                    mmucr.lrui |= LRUIValues[idx];
+
+                    const physical_address = entry.translate(virtual_addr);
+                    mmu_log.debug("ITLB Hit: {x:0>8} -> {x:0>8}", .{ virtual_addr, physical_address });
+                    mmu_log.debug("  Entry {d}: {any}", .{ idx, entry });
+                    return physical_address & 0x1FFF_FFFF;
                 }
-            },
+            }
         }
 
         // Fallback to UTLB
