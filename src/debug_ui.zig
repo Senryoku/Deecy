b28c2363da379e2932b35b7c24dd656fe7487125
Senryoku <maretverdant@gmail.com>
2025-03-14 02:33:11 +0100

    [debug_ui] Fix VPN display

--- src/debug_ui.zig
+++ src/debug_ui.zig
@@ -235,7 +235,7 @@ fn display_tlb(comptime name: [:0]const u8, tlbs: []SH4Module.mmu.TLBEntry) void
             _ = zgui.tableSetColumnIndex(1);
             zgui.textColored(color, "{X: >2}", .{tlb.asid});
             _ = zgui.tableSetColumnIndex(2);
-            zgui.textColored(color, "{X:0>8}", .{@as(u32, tlb.vpn << 10)});
+            zgui.textColored(color, "{X:0>8}", .{@as(u32, tlb.vpn) << 10});
             _ = zgui.tableSetColumnIndex(3);
             zgui.textColored(color, "{X:0>8}", .{tlb._ppn});
             _ = zgui.tableSetColumnIndex(4);
