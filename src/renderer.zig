32e2aac1d51bb4a6d56087e84f19458254362178
Senryoku <maretverdant@gmail.com>
2025-03-25 11:36:42 +0100

    [renderer] Partially preserve draw call order
    Preserve some order between draw calls using different depth compare function. I started by keeping the absolute order,
    only batching the consecutive draw calls with exactly the same parameters, but this had a pretty heavy cost (~2x slower).
    Using depth compare function changes as 'barriers' seems to be enough to fix the current issues, and allows for much
    heavier batching, keeping most of the performance in the couple of games I tried.
    Not a fan of the CPU side though, it's even messier than before, and does a lot of allocations now.

--- src/renderer.zig
+++ src/renderer.zig
@@ -412,25 +412,26 @@ const PipelineMetadata = struct {
 
 const PassMetadata = struct {
     pass_type: HollyModule.ListType,
-    pipelines: std.AutoArrayHashMap(PipelineKey, PipelineMetadata),
+    steps: std.ArrayList(std.AutoArrayHashMap(PipelineKey, PipelineMetadata)),
 
     pub fn init(allocator: std.mem.Allocator, pass_type: HollyModule.ListType) PassMetadata {
         return .{
             .pass_type = pass_type,
-            .pipelines = .init(allocator),
+            .steps = .init(allocator),
         };
     }
 
     fn deinit(self: *@This()) void {
-        for (self.pipelines.values()) |*pipeline|
-            pipeline.deinit();
-        self.pipelines.deinit();
+        self.reset();
+        self.steps.deinit();
     }
 
     pub fn reset(self: *@This()) void {
-        for (self.pipelines.values()) |*pipeline|
-            pipeline.deinit();
-        self.pipelines.clearRetainingCapacity();
+        for (self.steps.items) |*step| {
+            for (step.values()) |*pipeline|
+                pipeline.deinit();
+        }
+        self.steps.clearRetainingCapacity();
     }
 };
 
@@ -2133,20 +2134,8 @@ pub const Renderer = struct {
 
             const ta_lists = &self.ta_lists_to_render.items[pass_idx];
 
-            for ([3]*PassMetadata{ &render_pass.opaque_pass, &render_pass.punchthrough_pass, &render_pass.translucent_pass }) |pass| {
-                // NOTE/FIXME: We're never purging the draw calls list. Right now we can only have at most one draw call per sampler type (i.e. 3 * 3 * 2 = 18),
-                //             which is okay, I think. However this might become problematic down the line.
-                //             We're saving a lot of allocations this way, but there's probably a better way to do it.
-                var it = pass.pipelines.iterator();
-                while (it.next()) |pipeline| {
-                    for (pipeline.value_ptr.*.draw_calls.values()) |*draw_call| {
-                        draw_call.start_index = 0;
-                        draw_call.index_count = 0;
-                    }
-                }
-            }
+            render_pass.clearRetainingCapacity();
 
-            render_pass.pre_sorted_translucent_pass.clearRetainingCapacity();
             pre_sorted_indices.clearRetainingCapacity();
             var pre_sorted_index_offset: u32 = 0;
 
@@ -2157,6 +2146,9 @@ pub const Renderer = struct {
                 var area1_face_color: fARGB = undefined;
                 var area1_face_offset_color: fARGB = undefined;
                 const display_list: *const HollyModule.DisplayList = @constCast(ta_lists).get_list(list_type);
+
+                var current_depth_compare_function: ?wgpu.CompareFunction = null;
+                var current_step: *std.AutoArrayHashMap(PipelineKey, PipelineMetadata) = undefined;
 
                 for (0..display_list.vertex_strips.items.len) |idx| {
                     const strip_first_vertex_index: usize = self.vertices.items.len;
@@ -2614,9 +2606,19 @@ pub const Renderer = struct {
                                     else => @compileError("Invalid list type"),
                                 };
 
-                                var pipeline = pass.pipelines.getPtr(pipeline_key) orelse put: {
-                                    try pass.pipelines.put(pipeline_key, .init(self._allocator));
-                                    break :put pass.pipelines.getPtr(pipeline_key).?;
+                                if (current_depth_compare_function == null) {
+                                    current_depth_compare_function = pipeline_key.depth_compare;
+                                    try pass.steps.append(.init(self._allocator));
+                                    current_step = &pass.steps.items[0];
+                                } else if (current_depth_compare_function != pipeline_key.depth_compare) {
+                                    current_depth_compare_function = pipeline_key.depth_compare;
+                                    try pass.steps.append(.init(self._allocator));
+                                    current_step = &pass.steps.items[pass.steps.items.len - 1];
+                                }
+
+                                var pipeline = current_step.getPtr(pipeline_key) orelse put: {
+                                    try current_step.put(pipeline_key, .init(self._allocator));
+                                    break :put current_step.getPtr(pipeline_key).?;
                                 };
 
                                 const draw_call_key = DrawCallKey{ .sampler = sampler, .user_clip = display_list.vertex_strips.items[idx].user_clip };
@@ -2651,15 +2653,17 @@ pub const Renderer = struct {
 
             if (self.vertices.items.len > 0) {
                 for ([3]*PassMetadata{ &render_pass.opaque_pass, &render_pass.punchthrough_pass, &render_pass.translucent_pass }) |pass| {
-                    var it = pass.pipelines.iterator();
-                    while (it.next()) |entry| {
-                        for (entry.value_ptr.*.draw_calls.values()) |*draw_call| {
-                            draw_call.start_index = index_buffer_pointer;
-                            draw_call.index_count = @intCast(draw_call.indices.items.len);
-                            self._gctx.queue.writeBuffer(index_buffer, @sizeOf(u32) * index_buffer_pointer, u32, draw_call.indices.items);
-                            index_buffer_pointer += @intCast(draw_call.indices.items.len);
+                    for (pass.steps.items) |*step| {
+                        var it = step.iterator();
+                        while (it.next()) |entry| {
+                            for (entry.value_ptr.*.draw_calls.values()) |*draw_call| {
+                                draw_call.start_index = index_buffer_pointer;
+                                draw_call.index_count = @intCast(draw_call.indices.items.len);
+                                self._gctx.queue.writeBuffer(index_buffer, @sizeOf(u32) * index_buffer_pointer, u32, draw_call.indices.items);
+                                index_buffer_pointer += @intCast(draw_call.indices.items.len);
 
-                            draw_call.indices.clearRetainingCapacity();
+                                draw_call.indices.clearRetainingCapacity();
+                            }
                         }
                     }
                 }
@@ -2925,21 +2929,22 @@ pub const Renderer = struct {
                         // Opaque and PunchThrough geometry
                         // FIXME: PunchThrough should be drawn last? Is there a case where it matters with this setup?
                         inline for ([2]*const PassMetadata{ &render_pass.opaque_pass, &render_pass.punchthrough_pass }) |metadata| {
-                            var it = metadata.pipelines.iterator();
-                            while (it.next()) |entry| {
-                                // FIXME: We should also check if at least one of the draw calls is not empty (we're keeping them around even if they are empty right now).
-                                if (entry.value_ptr.*.draw_calls.count() > 0) {
-                                    const pl = self.get_or_put_pipeline(entry.key_ptr.*, .Async);
-                                    const pipeline = gctx.lookupResource(pl) orelse continue;
-                                    pass.setPipeline(pipeline);
+                            for (metadata.steps.items) |step| {
+                                var it = step.iterator();
+                                while (it.next()) |entry| {
+                                    if (entry.value_ptr.*.draw_calls.count() > 0) {
+                                        const pl = self.get_or_put_pipeline(entry.key_ptr.*, .Async);
+                                        const pipeline = gctx.lookupResource(pl) orelse continue;
+                                        pass.setPipeline(pipeline);
 
-                                    for (entry.value_ptr.*.draw_calls.values()) |draw_call| {
-                                        if (draw_call.index_count > 0) {
-                                            const clip = self.convert_clipping(draw_call.user_clip);
-                                            pass.setScissorRect(clip.x, clip.y, clip.width, clip.height);
+                                        for (entry.value_ptr.*.draw_calls.values()) |draw_call| {
+                                            if (draw_call.index_count > 0) {
+                                                const clip = self.convert_clipping(draw_call.user_clip);
+                                                pass.setScissorRect(clip.x, clip.y, clip.width, clip.height);
 
-                                            pass.setBindGroup(1, gctx.lookupResource(self.sampler_bind_groups[draw_call.sampler]).?, &.{});
-                                            pass.drawIndexed(draw_call.index_count, 1, draw_call.start_index, 0, 0);
+                                                pass.setBindGroup(1, gctx.lookupResource(self.sampler_bind_groups[draw_call.sampler]).?, &.{});
+                                                pass.drawIndexed(draw_call.index_count, 1, draw_call.start_index, 0, 0);
+                                            }
                                         }
                                     }
                                 }
@@ -3144,7 +3149,7 @@ pub const Renderer = struct {
 
                         // TODO: Support translucent modifier volumes in pre-sorted mode
 
-                    } else {
+                    } else if (render_pass.translucent_pass.steps.items.len > 0) {
                         // Generate all translucent fragments
                         const translucent_bind_group = gctx.lookupResource(self.translucent_bind_group).?;
                         const blend_bind_group = if (render_to_texture) gctx.lookupResource(self.blend_bind_group_render_to_texture).? else gctx.lookupResource(self.blend_bind_group).?;
@@ -3261,20 +3266,22 @@ pub const Renderer = struct {
                                 pass.setBindGroup(0, bind_group, &.{uniform_mem.offset});
                                 pass.setBindGroup(2, translucent_bind_group, &.{oit_uniform_mem.offset});
 
-                                var it = render_pass.translucent_pass.pipelines.iterator();
-                                while (it.next()) |entry| {
-                                    if (entry.value_ptr.*.draw_calls.count() > 0) {
-                                        for (entry.value_ptr.*.draw_calls.values()) |draw_call| {
-                                            if (draw_call.index_count > 0) {
-                                                var clip = self.convert_clipping(draw_call.user_clip);
-                                                const min_max_y = @min(clip.y + clip.height, start_y + slice_size);
-                                                clip.y = @max(clip.y, start_y);
-                                                clip.height = if (min_max_y > clip.y) min_max_y - clip.y else 0;
-                                                if (clip.height > 0 and clip.width > 0) {
-                                                    pass.setScissorRect(clip.x, clip.y, clip.width, clip.height);
+                                for (render_pass.translucent_pass.steps.items) |step| {
+                                    var it = step.iterator();
+                                    while (it.next()) |entry| {
+                                        if (entry.value_ptr.*.draw_calls.count() > 0) {
+                                            for (entry.value_ptr.*.draw_calls.values()) |draw_call| {
+                                                if (draw_call.index_count > 0) {
+                                                    var clip = self.convert_clipping(draw_call.user_clip);
+                                                    const min_max_y = @min(clip.y + clip.height, start_y + slice_size);
+                                                    clip.y = @max(clip.y, start_y);
+                                                    clip.height = if (min_max_y > clip.y) min_max_y - clip.y else 0;
+                                                    if (clip.height > 0 and clip.width > 0) {
+                                                        pass.setScissorRect(clip.x, clip.y, clip.width, clip.height);
 
-                                                    pass.setBindGroup(1, gctx.lookupResource(self.sampler_bind_groups[draw_call.sampler]).?, &.{});
-                                                    pass.drawIndexed(draw_call.index_count, 1, draw_call.start_index, 0, 0);
+                                                        pass.setBindGroup(1, gctx.lookupResource(self.sampler_bind_groups[draw_call.sampler]).?, &.{});
+                                                        pass.drawIndexed(draw_call.index_count, 1, draw_call.start_index, 0, 0);
+                                                    }
                                                 }
                                             }
                                         }
