a3975734458d804bea0aef425c354716c48e373e
Senryoku <maretverdant@gmail.com>
2025-12-13 08:48:37 +0100

    [ui] Small screenshotting refactor

--- src/deecy.zig
+++ src/deecy.zig
@@ -103,7 +103,16 @@ fn glfw_key_callback(
                         };
                         defer screen.deinit(app._allocator);
                         const filename = "screenshot.bmp";
-                        write_bmp(filename, screen.bgra, screen.width, screen.height) catch |err| {
+
+                        var file = std.fs.cwd().createFile(filename, .{}) catch |err| {
+                            deecy_log.err(termcolor.red("Failed to create screenshot file '{s}': {t}"), .{ filename, err });
+                            return;
+                        };
+                        defer file.close();
+
+                        var buffer: [1024]u8 = undefined;
+                        var file_writer = file.writer(&buffer);
+                        screen.write_bmp(&file_writer.interface) catch |err| {
                             deecy_log.err(termcolor.red("Failed to write screenshot file: {t}"), .{err});
                             return;
                         };
@@ -115,37 +124,6 @@ fn glfw_key_callback(
             }
         }
     }
-}
-
-fn write_bmp(filename: []const u8, bgra: []const u8, width: u32, height: u32) !void {
-    var file = try std.fs.cwd().createFile(filename, .{});
-    defer file.close();
-
-    var buffer: [1024]u8 = undefined;
-    var file_writer = file.writer(&buffer);
-    const writer = &file_writer.interface;
-
-    try writer.writeAll("BM");
-    try writer.writeInt(u32, 54 + 4 * width * height, .little); // File size
-    try writer.writeInt(u16, 0, .little);
-    try writer.writeInt(u16, 0, .little);
-    try writer.writeInt(u32, 54, .little); // Pixel data offset
-
-    try writer.writeInt(u32, 40, .little); // Header Size
-    try writer.writeInt(u32, width, .little);
-    try writer.writeInt(i32, -@as(i32, @intCast(height)), .little); // Height, negative for top to bottom
-    try writer.writeInt(u16, 1, .little); // Planes
-    try writer.writeInt(u16, 32, .little); // Bits per pixels
-    try writer.writeInt(u32, 0, .little); // Compression: BI_RGB
-    try writer.writeInt(u32, 0, .little); // Pixel data size, can be 0 for BI_RGB
-    try writer.writeInt(u32, 2835, .little); // Horizontal resolution of the image. (pixel per metre, signed integer)
-    try writer.writeInt(u32, 2835, .little); // Vertical resolution of the image. (pixel per metre, signed integer)
-    try writer.writeInt(u32, 0, .little); // Number of colors in the color palette
-    try writer.writeInt(u32, 0, .little); // Number of important colors
-
-    try writer.writeAll(bgra);
-
-    try writer.flush();
 }
 
 fn glfw_drop_callback(
--- src/image.zig
+++ src/image.zig
@@ -15,3 +15,27 @@ pub fn init(allocator: std.mem.Allocator, width: u32, height: u32) !@This() {
 pub fn deinit(self: @This(), allocator: std.mem.Allocator) void {
     allocator.free(self.bgra);
 }
+
+pub fn write_bmp(self: @This(), writer: *std.Io.Writer) !void {
+    try writer.writeAll("BM");
+    try writer.writeInt(u32, 54 + 4 * self.width * self.height, .little); // File size
+    try writer.writeInt(u16, 0, .little);
+    try writer.writeInt(u16, 0, .little);
+    try writer.writeInt(u32, 54, .little); // Pixel data offset
+
+    try writer.writeInt(u32, 40, .little); // Header Size
+    try writer.writeInt(u32, self.width, .little);
+    try writer.writeInt(i32, -@as(i32, @intCast(self.height)), .little); // Height, negative for top to bottom
+    try writer.writeInt(u16, 1, .little); // Planes
+    try writer.writeInt(u16, 32, .little); // Bits per pixels
+    try writer.writeInt(u32, 0, .little); // Compression: BI_RGB
+    try writer.writeInt(u32, 0, .little); // Pixel data size, can be 0 for BI_RGB
+    try writer.writeInt(u32, 2835, .little); // Horizontal resolution of the image. (pixel per metre, signed integer)
+    try writer.writeInt(u32, 2835, .little); // Vertical resolution of the image. (pixel per metre, signed integer)
+    try writer.writeInt(u32, 0, .little); // Number of colors in the color palette
+    try writer.writeInt(u32, 0, .little); // Number of important colors
+
+    try writer.writeAll(self.bgra);
+
+    try writer.flush();
+}
