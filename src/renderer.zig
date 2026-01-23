e2179f7d05fbf90727f1ff4b00340c9c154b34b6
Senryoku <maretverdant@gmail.com>
2026-01-23 03:17:56 +0100

    [renderer] Render a late frame immediately when receiving a render-to-texture request rather than skipping it and turning synchronous render on automatically.
    Next commit will turn the experimental 'Render in Emulation Thread' setting into a proper, saved, setting. This would be weird to have it enable itself randomly.

--- src/renderer.zig
+++ src/renderer.zig
@@ -1639,12 +1639,14 @@ pub const Renderer = struct {
         if (self.render_request and !render_to_texture)
             log.warn(termcolor.yellow("Woops! Skipped a frame."), .{});
         if (self.render_request and render_to_texture) {
-            log.warn(termcolor.yellow("Render to a texture with a pending render request. Force enabling Render on Emulation Thread."), .{});
+            log.warn(termcolor.yellow("Render to a texture with a pending render request. Consider enabling 'Synchronous Rendering'."), .{});
             // NOTE: This has a greater chance of happening with the frame limiter enabled. Render requests can easily be delayed by a frame.
-            // We cannot simply wait on the main thread here, as we could end up in a deadlock (On exit for example: main thread joining on the emulation thread, while the emulation thread is waiting on the main thread to finish rendering).
-            // So for now we'll guarantee order of operations by rendering everything in the emulation thread. FIXME: If exiting is the only problematic case, and we're careful, waiting might be a better option.
-            self.render_request = false; // Cancel the render request, it will be invalid after this render-to-texture anyway.
-            self.ExperimentalRenderOnEmulationThread = true;
+            //       We cannot simply wait on the main thread here, as we could end up in a deadlock (On exit for example: main thread joining on the emulation thread, while the emulation thread is waiting on the main thread to finish rendering).
+            //       So for now we'll guarantee order of operations by rendering the late frames right here.
+            // Render immediately
+            self.render_request = false;
+            self.update(&dc.gpu) catch |err| return log.err(termcolor.red("Failed to update renderer: {t}"), .{err});
+            self.render(&dc.gpu, render_to_texture) catch |err| return log.err(termcolor.red("Failed to render: {t}"), .{err});
         }
 
         self.on_render_start_param_base = dc.gpu.read_register(u32, .PARAM_BASE);
