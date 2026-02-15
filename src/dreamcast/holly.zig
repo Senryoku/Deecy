Senryoku/Deecy

commit 1e926e6bc43c97e566dc6fd37b3864226ad710be

[holly] Fixed a regression in interlace handling.
Make sure to hit all scanlines and alternate fields (scanline parity).

src/dreamcast/holly.zig
@@ -1692,8 +1692,11 @@ pub const Holly = struct {
                 if (spg_control.interlace) {
                     target_scanline += 2 * @as(u64, line_count);
                     if (target_scanline >= max_scanline) {
-                        target_scanline += (target_scanline / max_scanline) % 2; // Next field
+                        const field = target_scanline & 1;
+                        const full_scans = target_scanline / max_scanline;
                         target_scanline %= max_scanline;
+                        target_scanline &= ~@as(u64, 1);
+                        target_scanline |= (full_scans + field) % 2; // Next field
                     }
                 } else {
                     target_scanline += line_count;

src/dreamcast/sh4.zig
@@ -1306,7 +1306,7 @@ pub const SH4 = struct {
                             // an input, the external pin value sampled on the external bus clock is read. When a bit is set as an
                             // output, the value written to the PDTRA register is read.
 
-                            var out: u16 = @as(u16, @intFromEnum(self._dc.?.cable_type)) << 8;
+                            var out: u16 = @intFromEnum(self._dc.?.cable_type) << 8;
 
                             const ctrl: u32 = self.read_p4_register(u32, .PCTRA) & 0xF;
                             const data: u16 = self.read_p4_register(u16, .PDTRA) & 0xF;

commit 088d167ecb6aea142cad0499a3aeffc331e1dc02

[holly] Fix VBlankOut interrupt scheduling.
Was doubling interrupts in some very specific cases, messing with maple DMA in Armada (regression introduced in 1e926e6bc43c97e566dc6fd37b3864226ad710be).

src/dreamcast/holly.zig
@@ -1698,7 +1698,7 @@ pub const Holly = struct {
         else
             (if (max_scanline >= spg_status.scanline) max_scanline - spg_status.scanline else 0) + target_scanline;
         if (line_diff == 0) line_diff = max_scanline;
-        if (spg_control.interlace) line_diff = @max(line_diff / 2, 1);
+        if (spg_control.interlace) line_diff = @max((line_diff + 1) / 2, 1);
         const hcount: u64 = spg_load.hcount;
         const sh4_cycles = self.pixels_to_sh4_cycles((hcount + 1) * line_diff);
         std.debug.assert(sh4_cycles > 0);